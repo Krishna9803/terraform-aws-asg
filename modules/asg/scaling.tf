890d19ec0e1a73aaa8233b74e9e00336a134a105
Krishna9803 <64780071+Krishna9803@users.noreply.github.com>
2025-05-27 12:51:13 +0530

    Update scaling.tf

--- modules/asg/scaling.tf
+++ modules/asg/scaling.tf
@@ -1,15 +1,37 @@
 resource "aws_autoscaling_policy" "scale_up" {
   name                   = "${var.name_prefix}-scale-up"
-  scaling_adjustment     = 1
+  policy_type            = "SimpleScaling"
   adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = 1
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.this.name
 }
 
 resource "aws_autoscaling_policy" "scale_down" {
   name                   = "${var.name_prefix}-scale-down"
-  scaling_adjustment     = -1
+  policy_type            = "SimpleScaling"
   adjustment_type        = "ChangeInCapacity"
-  cooldown               = 600
+  scaling_adjustment     = -1
+  cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.this.name
+
+
+}
+
+
+resource "aws_autoscaling_policy" "target_tracking" {
+  name                   = "${var.name_prefix}-target-tracking"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.this.name
+
+  target_tracking_configuration {
+    target_value = 40.0 
+    disable_scale_in = false
+
+    customized_metric_specification {
+      metric_name = "CPUUtilization"
+      namespace   = "AWS/EC2"
+      statistic   = "Average"
+    }
+  }
 }
