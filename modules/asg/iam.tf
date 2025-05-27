277dc343cf8baaeecf7b3671679cf6fd8e222cab
Krishna9803 <64780071+Krishna9803@users.noreply.github.com>
2025-05-27 11:16:29 +0530

    Update iam.tf

--- modules/asg/iam.tf
+++ modules/asg/iam.tf
@@ -1,99 +1,113 @@
-resource "aws_iam_role" "ec2" {
-  name_prefix        = "${var.name_prefix}-ec2-role-"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
+provider "aws"{
+    region = "us-east-1"
 }
 
-resource "aws_iam_instance_profile" "ec2" {
-  name_prefix = "${var.name_prefix}-ec2-profile-"
-  role        = aws_iam_role.ec2.name
+resource "aws_launch_template" "this" {
+  name     = "test-asg-lt"
+  image_id      = var.image_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  user_data     = var.user_data != null ? base64encode(var.user_data) : null
+
+  vpc_security_group_ids = var.security_group_ids
+  ebs_optimized          = var.ebs_optimized
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = var.root_volume_size
+      volume_type           = var.root_volume_type
+      encrypted             = var.root_volume_encrypted
+      delete_on_termination = var.enable_hibernation ? false : true
+    }
+  } 
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2.name
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = merge(var.tags, { Name = "${var.name_prefix}-instance" })
+  }
+
+  hibernation_options {
+    configured = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-data "aws_iam_policy_document" "ec2_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+resource "aws_autoscaling_group" "this" {
+  name_prefix         = "${var.name_prefix}-asg-"
+  max_size            = var.max_size
+  min_size            = var.min_size
+  desired_capacity    = var.desired_capacity
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.this.id
+    version = "$Latest"
+  }
+
+  target_group_arns    = var.target_group_arns
+  health_check_type    = var.health_check_type
+  termination_policies = var.termination_policies
+
+  dynamic "initial_lifecycle_hook" {
+    for_each = var.initial_lifecycle_hooks
+    content {
+      name                    = initial_lifecycle_hook.value.name
+      lifecycle_transition    = initial_lifecycle_hook.value.transition
+      default_result          = lookup(initial_lifecycle_hook.value, "default_result", null)
+      heartbeat_timeout       = lookup(initial_lifecycle_hook.value, "heartbeat_timeout", null)
+      notification_target_arn = lookup(initial_lifecycle_hook.value, "notification_arn", null)
+      role_arn                = lookup(initial_lifecycle_hook.value, "role_arn", null)
     }
   }
-}
 
-resource "aws_iam_role_policy" "ec2" {
-  name_prefix = "ec2-permissions-"
-  role        = aws_iam_role.ec2.name
-  policy      = data.aws_iam_policy_document.ec2_permissions.json
-}
+  dynamic "warm_pool" {
+    for_each = var.enable_hibernation ? [1] : []
+    content {
+      pool_state = "Hibernated"
+      min_size   = var.warm_pool_min_size
 
-data "aws_iam_policy_document" "ec2_permissions" {
-  statement {
-    actions = [
-      "ssm:GetParameter",
-      "s3:GetObject"
-    ]
-    resources = ["*"]
+      instance_reuse_policy {
+        reuse_on_scale_in = true
+      }
+    }
+  }
+
+  dynamic "tag" {
+    for_each = merge(var.tags, { Name = var.name_prefix })
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  lifecycle {
+    ignore_changes        = [desired_capacity, target_group_arns]
+    create_before_destroy = true
   }
 }
 
+# SNS Topic
+resource "aws_sns_topic" "lifecycle_notifications" {
+  name = "${var.name_prefix}-lifecycle-topic"
 
-
-
-resource "aws_iam_role" "lambda_exec" {
-  name_prefix = "${var.name_prefix}-lambda-"
-  assume_role_policy = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = { Service = ["lambda.amazonaws.com", "autoscaling.amazonaws.com"] }
-    }]
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = { Service = "autoscaling.amazonaws.com" },
+        Action    = "sns:Publish",
+        Resource  = "arn:aws:sns:${var.region}:${data.aws_caller_identity.current.account_id}:${var.name_prefix}-lifecycle-topic"
+      }
+    ]
   })
-}
-
-resource "aws_iam_policy" "lambda_hibernation" {
-  name_prefix = "${var.name_prefix}-lambda-hibernate-"
-  policy      = data.aws_iam_policy_document.lambda_permissions.json
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_custom" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy.lambda_hibernation.arn
-}
-
-data "aws_caller_identity" "current" {}
-
-
-#to pub sns topic
-
-data "aws_iam_policy_document" "lambda_permissions" {
-  statement {
-    actions = [
-      "autoscaling:CompleteLifecycleAction",
-      "autoscaling:RecordLifecycleActionHeartbeat",
-      "ec2:StopInstances",
-      "ec2:DescribeInstances",
-      "sns:Publish"
-    ]
-    resources = [
-      "arn:aws:autoscaling:${var.region}:${data.aws_caller_identity.current.account_id}:autoScalingGroup:*:autoScalingGroupName/${var.name_prefix}-*",
-      "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id}:instance/*",
-      aws_sns_topic.lifecycle_notifications.arn 
-    ]
-  }
-}
-
-
-data "aws_iam_policy_document" "sns_topic_policy" {
-  statement {
-    actions = ["sns:Publish"]
-    principals {
-      type        = "Service"
-      identifiers = ["autoscaling.amazonaws.com"]
-    }
-    resources = [aws_sns_topic.lifecycle_notifications.arn]
-  }
 }
