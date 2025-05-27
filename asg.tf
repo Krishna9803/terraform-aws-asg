8dc621b7f1ddda740e6d1b2ffcd6a4b9774a0ca1
Krishna9803 <64780071+Krishna9803@users.noreply.github.com>
2025-05-27 11:14:28 +0530

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,36 +1,35 @@
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "*********************"
+  secret_key = "********************************"
 }
 
 module "autoscaling" {
   source = "./modules/asg"
 
   name_prefix        = "test-asg"
-  image_id           = "ami-40128086412120"
+  image_id           = "ami-0953476d60561c955"
   instance_type      = "t3.micro"
-  security_group_ids = ["sg-1234567890abcdef0"]
-  subnet_ids         = ["subnet-12345", "subnet-67890"]
-
+  security_group_ids = ["sg-043a1188ef4814999"]
+  subnet_ids = [
+    "subnet-0cb91dabc10a32bd5", # public - us-east-1a
+    "subnet-0bcd7932ffa8ffa51",  # us-east-1b, public
+  ]
   min_size         = 2
   max_size         = 5
   desired_capacity = 3
 
-  #   cpu_scale_up_threshold   = 70
-  #   cpu_scale_down_threshold = 30
+  ######
+  # # Optional: Attach to a Load Balancer Target Group
+  # target_group_arns = ["arn:aws:elasticloadbalancing:us-east-1:123456789012:targetgroup/my-tg/abcdef1234567890"]
 
-  #   schedule_scale_up_recurrence   = "0 9 * * MON-FRI"
-  #   schedule_scale_down_recurrence = "0 18 * * MON-FRI"
+  # # Optional: User data
+  # user_data = <<-EOF
+  #   #!/bin/bash
+  #   echo "Hello from ASG" > /tmp/asg.txt
+  # EOF
 
-  # Optional: Attach to a Load Balancer Target Group
-  target_group_arns = ["arn:aws:elasticloadbalancing:us-east-1:123456789012:targetgroup/my-tg/abcdef1234567890"]
-
-  # Optional: User data
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "Hello from ASG" > /tmp/asg.txt
-  EOF
-
-  key_name = "my-keypair"
+  # key_name = "my-keypair"
 
   # Optional: EBS settings
   ebs_optimized         = false
@@ -40,17 +39,17 @@ module "autoscaling" {
 
   # Optional: Health checks and termination
   health_check_type    = "EC2"
-  termination_policies = ["OldestInstance"]
+  termination_policies = ["OldestLaunchTemplate"]
 
-  # Optional: Lifecycle hooks
-  initial_lifecycle_hooks = [
-    {
-      name              = "launch-wait"
-      transition        = "autoscaling:EC2_INSTANCE_LAUNCHING"
-      default_result    = "CONTINUE"
-      heartbeat_timeout = 300
-    }
-  ]
+  # # Optional: Lifecycle hooks
+  # initial_lifecycle_hooks = [
+  #   {
+  #     name              = "launch-wait"
+  #     transition        = "autoscaling:EC2_INSTANCE_LAUNCHING"
+  #     default_result    = "CONTINUE"
+  #     heartbeat_timeout = 300
+  #   }
+  # ]
 
   # Optional: Scaling policies thresholds
   cpu_scale_up_threshold   = 70
@@ -77,7 +76,3 @@ module "autoscaling" {
     Project     = "asg-demo"
   }
 }
-
-
-
-
