63fd1b39e7700904728da5ebaedf8ccf7acb6dc0
Krishna9803 <64780071+Krishna9803@users.noreply.github.com>
2025-05-27 11:18:17 +0530

    Update scaling.tf

--- modules/asg/scaling.tf
+++ modules/asg/scaling.tf
@@ -10,6 +10,6 @@ resource "aws_autoscaling_policy" "scale_down" {
   name                   = "${var.name_prefix}-scale-down"
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
+  cooldown               = 600
   autoscaling_group_name = aws_autoscaling_group.this.name
 }
