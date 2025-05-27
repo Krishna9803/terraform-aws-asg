8978a1fc8e00d996762203826241e1cb9bb062f8
Krishna9803 <64780071+Krishna9803@users.noreply.github.com>
2025-05-27 12:51:27 +0530

    Update schedule_scaling.tf

--- modules/asg/schedule_scaling.tf
+++ modules/asg/schedule_scaling.tf
@@ -1,17 +1,20 @@
 resource "aws_autoscaling_schedule" "scale_up" {
   scheduled_action_name  = "${var.name_prefix}-scale-up"
   autoscaling_group_name = aws_autoscaling_group.this.name
-  min_size               = var.schedule_scale_up_min
-  max_size               = var.schedule_scale_up_max
-  desired_capacity       = var.schedule_scale_up_desired
-  recurrence             = var.schedule_scale_up_recurrence
+  # Hardcoded values
+  min_size         = 2
+  max_size         = 4
+  desired_capacity = 3
+  recurrence       = "0 9 * * MON-FRI"  # Mon-Fri 9 AM UTC
 }
 
 resource "aws_autoscaling_schedule" "scale_down" {
   scheduled_action_name  = "${var.name_prefix}-scale-down"
   autoscaling_group_name = aws_autoscaling_group.this.name
-  min_size               = var.schedule_scale_down_min
-  max_size               = var.schedule_scale_down_max
-  desired_capacity       = var.schedule_scale_down_desired
-  recurrence             = var.schedule_scale_down_recurrence
+  # Hardcoded values
+  min_size         = 1
+  max_size         = 2
+  desired_capacity = 1
+  recurrence       = "0 18 * * MON-FRI"  # Mon-Fri 6 PM UTC
 }
+
