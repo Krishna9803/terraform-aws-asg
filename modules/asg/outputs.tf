bff45dece117582ac96dfc1db17a2aa16a7d45f7
Krishna9803 <64780071+Krishna9803@users.noreply.github.com>
2025-05-27 11:17:21 +0530

    Update outputs.tf

--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -2,6 +2,9 @@ output "autoscaling_group_name" {
   value = aws_autoscaling_group.this.name
 }
 
+output "sns_topic_arn" {
+  value = aws_sns_topic.lifecycle_notifications.arn
+}
 
 
 output "lambda_function_name" {
