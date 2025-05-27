a5e86700074401454df75c6b29428ac6d03533b1
Krishna9803 <64780071+Krishna9803@users.noreply.github.com>
2025-05-27 11:16:16 +0530

    Update iam.tf

--- modules/asg/iam.tf
+++ modules/asg/iam.tf
@@ -37,7 +37,6 @@ data "aws_iam_policy_document" "ec2_permissions" {
 
 
 
-
 resource "aws_iam_role" "lambda_exec" {
   name_prefix = "${var.name_prefix}-lambda-"
   assume_role_policy = jsonencode({
@@ -45,7 +44,7 @@ resource "aws_iam_role" "lambda_exec" {
     Statement = [{
       Action    = "sts:AssumeRole",
       Effect    = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" }
+      Principal = { Service = ["lambda.amazonaws.com", "autoscaling.amazonaws.com"] }
     }]
   })
 }
@@ -65,13 +64,36 @@ resource "aws_iam_role_policy_attachment" "lambda_custom" {
   policy_arn = aws_iam_policy.lambda_hibernation.arn
 }
 
+data "aws_caller_identity" "current" {}
+
+
+#to pub sns topic
+
 data "aws_iam_policy_document" "lambda_permissions" {
   statement {
     actions = [
       "autoscaling:CompleteLifecycleAction",
+      "autoscaling:RecordLifecycleActionHeartbeat",
       "ec2:StopInstances",
-      "ec2:DescribeInstances"
+      "ec2:DescribeInstances",
+      "sns:Publish"
     ]
-    resources = ["*"]
+    resources = [
+      "arn:aws:autoscaling:${var.region}:${data.aws_caller_identity.current.account_id}:autoScalingGroup:*:autoScalingGroupName/${var.name_prefix}-*",
+      "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id}:instance/*",
+      aws_sns_topic.lifecycle_notifications.arn 
+    ]
+  }
+}
+
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    actions = ["sns:Publish"]
+    principals {
+      type        = "Service"
+      identifiers = ["autoscaling.amazonaws.com"]
+    }
+    resources = [aws_sns_topic.lifecycle_notifications.arn]
   }
 }
